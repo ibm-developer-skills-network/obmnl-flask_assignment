40cbf69aff14c1481211e63c07f123503c68bad4
vicky-playground <90204593+vicky-playground@users.noreply.github.com>
2023-07-20 01:11:38 -0400

    add exercises and redesign the pages

--- app.py
+++ app.py
@@ -4,52 +4,27 @@ from flask import Flask, redirect, request, render_template, url_for
 app = Flask(__name__)
 
 # Sample data
-transactions = [
-    {'id': 1, 'date': '2023-06-01', 'amount': 100},
-    {'id': 2, 'date': '2023-06-02', 'amount': -200},
-    {'id': 3, 'date': '2023-06-03', 'amount': 300}
-]
+
 
 # ---- Task 1 ----#
 # Read operation
 
 
-# Create operation: Display add transaction form
-@app.route("/add", methods=["GET", "POST"])
-def add_transaction():
-    if request.method == 'POST':
-        # Create a new transaction object using form field values
-        transaction = {
-            'id': len(transactions) + 1,
-            'date': request.form['date'],
-            'amount': float(request.form['amount'])
-        }
-        # Append the new transaction to the list
-        transactions.append(transaction)
-
-        # Redirect to the transactions list page
-        return redirect(url_for("get_transactions"))
-    
-    # Render the form template to display the add transaction form
-    return render_template("form.html")
-
-
-# Delete operation: Delete a transaction
-@app.route("/delete/<int:transaction_id>")
-def delete_transaction(transaction_id):
-    # Find the transaction with the matching ID and remove it from the list
-    for transaction in transactions:
-        if transaction['id'] == transaction_id:
-            transactions.remove(transaction)
-            break
-
-    # Redirect to the transactions list page
-    return redirect(url_for("get_transactions"))
 
 # ---- Task 2 ----#
+# Create operation
+
+
+
+# ---- Task 3 ----#
 # Update operation
 
 
-if __name__ == "__main__":
-    # Run the Flask app
-    app.run(debug=True)
+
+# ---- Task 4 ----#
+# Delete operation
+
+
+
+# Run the Flask app
+    
