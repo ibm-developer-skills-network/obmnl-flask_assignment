7d202acb5c33d11661a318ab479a4aa8ef1d09c6
vicky-playground <90204593+vicky-playground@users.noreply.github.com>
2023-07-20 02:01:39 -0400

    minor change

--- app.py
+++ app.py
@@ -4,26 +4,13 @@
 
 # Sample data
 
-
-# ---- Task 1 ----#
 # Read operation
 
-
-
-# ---- Task 2 ----#
 # Create operation
 
-
-
-# ---- Task 3 ----#
 # Update operation
 
-
-
-# ---- Task 4 ----#
 # Delete operation
-
-
 
 # Run the Flask app
     
