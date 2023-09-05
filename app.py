50c6518c179599a8f412ee8f3bca3bfcff67210f
23BThomas <bettysamthomas@outlook.com>
2023-09-05 11:40:35 -0400

    Completed CRUD application in Flask and add search transactions and total balance

--- app.py
+++ app.py
@@ -1,16 +1,104 @@
 # Import libraries
+from flask import Flask, request,url_for,redirect,render_template
 
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
-# Read operation
+# Read operation: List all transactions
+@app.route("/")
+def get_transactions():
+    return render_template("transactions.html", transactions=transactions)
 
-# Create operation
+# Create operation: Display add transaction form
+@app.route("/add", methods=["GET", "POST"])
+def add_transaction():
+    if request.method == 'POST':
+        # Create a new transaction object using form field values   
+        transaction = {
+              'id': len(transactions)+1,
+              'date': request.form['date'],
+              'amount': float(request.form['amount'])
+             }
+        
+        # Append the new transaction to the list
+        transactions.append(transaction)
+        
+        # Redirect to the transactions list page
+        return redirect(url_for("get_transactions"))
+    
+    # Render the form template to display the add transaction form
+    return render_template("form.html")
 
-# Update operation
+# Update operation: Display edit transaction form
+@app.route("/edit/<int:transaction_id>",methods= ["GET", "POST"])
+def edit_transaction(transaction_id):
+    if request.method == "POST":
+        # Extract the updated values from the form fields
+        date = request.form['date']
+        amount = float(request.form['amount'])
+        
+        # Find the transaction with the matching ID and update its values
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                transaction['date'] = date
+                transaction['amount'] = amount
+                break
+        
+        # Redirect to the transactions list page
+        return redirect(url_for("get_transactions"))
+    
+    # Find the transaction with the matching ID and render the edit form
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            return render_template("edit.html", transaction=transaction)
 
-# Delete operation
+# Delete operation: Delete a transaction
+@app.route("/delete/<int:transaction_id>")
+def delete_transaction(transaction_id):
+    # Find the transaction with the matching ID and remove it from the list
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            transactions.remove(transaction)
+            break
+
+    # Redirect to the transactions list page
+    return redirect(url_for("get_transactions"))
+
+#Search Operation: Search transaction form
+@app.route("/search",methods= ["GET","POST"])
+def search_transactions():
+    if request.method == "GET":
+    # Redirect to the transactions list page
+        return render_template('search.html')
+        
+    # Creating a new list that contains only the transactions whose amount falls within the specified range. 
+    minamount = float(request.form['min_amount'])
+    maxamount = float(request.form['max_amount'])
+    filtered_transactions = []
+
+    for transaction in transactions:
+        if transaction['amount'] >= minamount and \
+        transaction['amount'] <= maxamount:
+            filtered_transactions.append(transaction)
+    return render_template('transactions.html', transactions=filtered_transactions)
+
+
+#Total Balance: Displaying total balance
+@app.route("/balance")
+def total_balance():
+    balance = 0
+    for t in transactions:
+        balance = balance + t['amount']
+
+    return f'Total Balance: {balance}'
 
 # Run the Flask app
-    
+if __name__ == "__main__":
+    app.run(debug=True)  
